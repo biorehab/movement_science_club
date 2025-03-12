33f990e949b032f86f7cef3d0ac3e081aee2ee3f
Sivakumar Balasubramanian <siva82kb@gmail.com>
2025-03-12 19:09:27 +0530

    Setup the eleventy site.

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function(eleventyConfig) {
+    return {
+      dir: {
+        input: "src",  // Where our content is
+        output: "_site" // Where Eleventy will generate the site
+      }
+    };
+  };
---
+++ src/index.md
@@ -0,0 +1,6 @@
+---
+title: Home
+layout: base.njk
+---
+
+Hello, Eleventy! Welcome to my new blog.
